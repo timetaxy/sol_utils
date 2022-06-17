be87e2723f00473414e2742765d903eda394dcd8
Cloakd <thecloakdone@gmail.com>
2022-06-17 10:09:03 +0100

    Tidy up homepage

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -11,10 +11,6 @@
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 					<li class="nav-item active">
-						<router-link to="/" class="nav-link text-uppercase" href="#"><i class="fa fa-floppy-disk"></i> Drive <span class="sr-only">(current)</span>
-						</router-link>
-					</li>
-					<li class="nav-item active">
 						<router-link to="/" class="nav-link text-uppercase" href="#"><i class="fa fa-search"></i> Explorer <span class="sr-only">(current)</span>
 						</router-link>
 					</li>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,26 @@
 	<div>
 		<div class="row">
 
-			<div class="col-6 offset-3 mt25">
-				<form @submit="searchAddress">
+			<div class="col-6 offset-3 mt-5 text-center">
+
+				<h1 class="text-white mb-0">Arberling</h1>
+				<h3 class="grey">The Solana Arbitrage Tracker</h3>
+				<img class="mb-5 mt-3" src="@/assets/images/logo.png" alt="Arberling Logo">
+
+				<form class="mt-5" @submit="searchAddress">
 					<input v-model="form.search" class="form-control" type="text" placeholder="Address">
 
 					<div class="row">
-						<button type="submit" class="btn btn-outline-light btn-lg mt-3">SEARCH</button>
+						<button type="submit" class="btn btn-outline-light btn-lg my-4">SEARCH</button>
 					</div>
 				</form>
 
-				<div class="row text-white mt-3">
+				<div class="row text-white">
 					<div class="card">
 						<div class="card-body p-1">
+							<h6 class="text-white text-left">Recent Searches</h6>
 							<div v-for="(search, key) in recentSearches" :key="key">
-								<div class="recent-search p-2 my-2" @click="go(search)">{{search}}</div>
+								<div class="recent-search p-2 my-2" @click="go(search)">{{ search }}</div>
 							</div>
 						</div>
 					</div>
@@ -39,7 +45,7 @@ export default {
 		}
 	},
 	methods: {
-		go: function(search) {
+		go: function (search) {
 			this.form.search = search;
 			this.pushSearch(this.form.search)
 			this.$router.push(`/account/${this.form.search}`)
@@ -52,7 +58,7 @@ export default {
 			this.$router.push(`/account/${this.form.search}`)
 		},
 
-		pushSearch: function(search) {
+		pushSearch: function (search) {
 			const recent = this.getRecentSearches()
 			if (recent.includes(search)) {
 				return
@@ -62,7 +68,7 @@ export default {
 			localStorage.setItem("recentSearches", JSON.stringify(recent))
 		},
 
-		getRecentSearches: function(){
+		getRecentSearches: function () {
 			let recent = localStorage.getItem("recentSearches")
 			if (recent === null)
 				recent = []
@@ -79,9 +85,6 @@ export default {
 </script>
 
 <style scoped>
-.mt25 {
-	margin-top: 20%;
-}
 
 .recent-search {
 	border-radius: 7px;
